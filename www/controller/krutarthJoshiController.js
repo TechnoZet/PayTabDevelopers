c77f2ea1fdc7c823aaaf149ddeb25de91f953dda
Krutarth Joshi <Shreeganesh@Krutarths-MacBook-Pro.local>
2016-11-19 03:43:11 -0500

    jQuery masked input money and numeric keypad added

--- www/controller/krutarthJoshiController.js
+++ www/controller/krutarthJoshiController.js
@@ -1,4 +1,9 @@
 angular.module('krutarthJoshiController.module',[]).controller('krutarthJoshiController', function($scope,$location){
-	
+
 	$scope.developerName="Krutarth Joshi";
-}); 
+	
+	//jQuery Masked Money function
+	$scope.test=function(){
+		$("#demo1").maskMoney();
+	}
+});
