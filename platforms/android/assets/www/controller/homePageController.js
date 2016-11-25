9a1b94071aedcbc872562cf08a45f42e39eb4337
rbp54 <rbp54@njit.edu>
2016-11-25 16:00:46 -0500

    upload image

---
+++ platforms/android/assets/www/controller/homePageController.js
@@ -0,0 +1,25 @@
+angular.module('homePageController.module',[]).controller('homePageController', function($scope,$location){
+	
+	$scope.changeloc=function(val){
+		if(val=='1')
+		{
+		$location.url("/maulik");
+		}
+		else if(val=='2')
+		{
+			$location.url("/meet");
+		}
+		else if(val=='3')
+		{
+			$location.url("/krutarth");
+		}
+		else if(val=='4')
+		{
+			$location.url("/ritesh");
+		}
+		else
+			{
+				$location.url("/jaimil");
+			}
+	};
+}); 
---
+++ platforms/android/assets/www/js/app.js
@@ -0,0 +1,19 @@
+var PayTab = angular.module('PayTabDevelopers',['ngRoute','ngCordova', 'homePageController.module',
+												'router.module',
+												'maulikParikhController.module',
+												'meetShahController.module',
+												'krutarthJoshiController.module',
+												'riteshPatelController.module',
+												'jaimilShahController.module',
+											]);
+
+
+
+
+
+
+
+
+
+
+
